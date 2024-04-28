afbffc6a8e1340babc41af0d71f49c859772e1de
Sabine Kaiser <sabine@MBPvonSabine2.fritz.box>
2024-04-28 23:14:42 +0200

    Overwrite project

---
+++ docs/js/custom.js
@@ -0,0 +1,8 @@
+// Get Current Year
+function getCurrentYear() {
+    var d = new Date();
+    var year = d.getFullYear();
+    document.querySelector("#displayDateYear").innerText = year;
+}
+getCurrentYear()
+
